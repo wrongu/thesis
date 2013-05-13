374d71991ac1e8d1df9030785d2189f602b8ab2a
wrongu <richard.d.lange.13@dartmouth.edu>
2013-05-13 14:07:52 -0400

    trajectory affinities defined in terms of magnitude and angle

--- MoSeg_Matlab/createTrajectoryAffinityMatrix.m
+++ MoSeg_Matlab/createTrajectoryAffinityMatrix.m
@@ -32,49 +32,63 @@ if length(common_frames) > 1
         t5 = min(t+5-1, common_frames(end));
         t5_ind = t5-common_frames(1)+1;
         if ((t5-t+1 == 5) || ~computed_one) && (t ~= mosegParams.endframe)
-            F = flows{t}; u = F(:,:,1); v = F(:,:,2);
             % look at total displacement of points across 5 frames (or
             % however many frames are available)
             dispA = pointsA(:,t5_ind) - pointsA(:,t_ind);
             dispB = pointsB(:,t5_ind) - pointsB(:,t_ind);
-            sq_diff = sum((dispA - dispB) .* (dispA - dispB));
-            % normalization based on flow fields
-            sigmaA = 0; sigmaB = 0;
-            for tsub = t_ind:t5_ind
-                sigmaA = sigmaA + sum(arrayfun(@(f) flow_variation(u, v, ...
-                    pointsA(:,tsub), ...
-                    mosegParams.flow_variation_window), ...
-                    t:t5));
-                sigmaB = sigmaB + sum(arrayfun(@(f) flow_variation(u, v, ...
-                    pointsB(:,tsub), ...
-                    mosegParams.flow_variation_window), ...
-                    t:t5));
+            % we now have 2 vectors dispA and dispB that define the net
+            % motion of the 2 trajectories over 5 frames. These will be
+            % considered 'close' based on the two metrics of direction and
+            % magnitude similarity..
+            mA = norm(dispA);
+            mB = norm(dispB);
+            % 1) Direction similarity: cosine of angle between vectors,
+            if mA ~= 0 && mB ~= 0
+                % cosine computed as dot product of unit vectors
+                dsim = sum(dispA .* dispB) / (mA * mB);
+                % shift from range [-1, 1] to range [0, 1] so that 
+                %   180-degree opposition is given 0
+                dsim = 0.5 * dsim + 0.5;
+            else
+                % one vector was zero.. there can be no directional
+                % similarity
+                dsim = eps;
             end
-            sigma2 = min(sigmaA, sigmaB);
-            % distance computation
-            D = Dsp * sq_diff / (5 * sigma2);
+            % 2) Magnitude similarity
+            msim = 1 - abs(mA - mB) / (mA + mB);
+            % distance computation: spatial distance divided by direction-
+            % and magnitude-similarity. This assumes (maybe asserts) that
+            % 'distance' is inversely proportional to 'similarity'
+            D = Dsp / (eps + dsim * msim);
             if D > Dmax, Dmax = D; end
             computed_one = true;
         end
     end
+    % convert distance to affinity. Using a gaussian ensures that
+    % near-0 distance maps to near-1 affinity, and large magnitude distance
+    % maps to near-0 affinity
     affinity = exp(-mosegParams.lambda * Dmax);
     
     % DEBUG
-%     if rand < 0.01
-%         subplot(1,2,1);
-%         imshow(read(V, common_frames(1)));
-%         hold on;
-%         plot(TA.points(2,:), TA.points(1,:), '-or', ...
-%             TB.points(2,:), TB.points(1,:), '-ob');
-%         hold off;
-%         set(gca, 'YDir', 'reverse');
-%         axis([0 size(flows{1}, 2) 0 size(flows{1}, 1)]);
-%         subplot(1,2,2);
-%         imshow(flowToColor(flows{common_frames(1)}));
-%         fprintf('Dsp = %f\nsigma2 = %f\ndiff displacement = %g\naffinity = %g', ...
-%             Dsp, sigma2, sq_diff, affinity);
-%         pause;
-%     end
+    if rand < 0.01
+        subplot(1,2,1);
+        imshow(read(V, common_frames(1)));
+        hold on;
+        plot(TA.points(2,:), TA.points(1,:), '-or', ...
+            TB.points(2,:), TB.points(1,:), '-ob');
+        hold off;
+        set(gca, 'YDir', 'reverse');
+        axis([0 size(flows{1}, 2) 0 size(flows{1}, 1)]);
+        subplot(1,2,2);
+        imshow(flowToColor(flows{common_frames(1)}));
+        hold on;
+        plot(TA.points(2,:), TA.points(1,:), '-or', ...
+            TB.points(2,:), TB.points(1,:), '-ob');
+        hold off;
+        fprintf('Dsp = %f\ndir sim = %g\n mag sim = %g\nDmax = %f\naffinity = %f', ...
+            Dsp, dsim, msim, Dmax, affinity);
+        pause;
+    end
 else
     affinity = 0;
 end
