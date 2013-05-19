295ead6ca320aca8ee1694a92d091e77297eff7a
wrongu <richard.d.lange.13@dartmouth.edu>
2013-05-19 13:57:30 -0400

    parfor moved from learnTheta:domain to deriveLikelihood:posneg

--- objectness/MS/deriveLikelihoodMS.m
+++ objectness/MS/deriveLikelihoodMS.m
@@ -1,25 +1,27 @@
 function [likelihood, pobj] = deriveLikelihoodMS(posneg,params)
 
-examplesPos = zeros(length(posneg) * params.distribution_windows,1);
-examplesNeg = zeros(length(posneg) * params.distribution_windows,1);
+	examplesPosCell = cell(1, length(posneg));
+	examplesNegCell = cell(1, length(posneg));
+%examplesPos = zeros(length(posneg) * params.distribution_windows,1);
+%examplesNeg = zeros(length(posneg) * params.distribution_windows,1);
 
 pos = 0;
 neg = 0;
 
-for idx2 = 1:length(posneg)
+parfor idx = 1:length(posneg)
     
-    indexPositive = find(posneg(idx2).labels == 1);
-    examplesPos(pos+1:pos+length(indexPositive)) = posneg(idx2).scores(indexPositive);
+    indexPositive = find(posneg(idx).labels == 1);
+    examplesPosCell{idx} = posneg(idx).scores(indexPositive);
     pos = pos + length(indexPositive);
     
-    indexNegative = find(posneg(idx2).labels == -1);
-    examplesNeg(neg+1:neg+length(indexNegative)) = posneg(idx2).scores(indexNegative);
+    indexNegative = find(posneg(idx).labels == -1);
+    examplesNegCell{idx} = posneg(idx).scores(indexNegative);
     neg = neg + length(indexNegative);
     
 end
 
-examplesPos(pos+1:end) = [];
-examplesNeg(neg+1:end) = [];
+	examplesPos = vertcat(examplesPosCell{:});
+	examplesNeg = vertcat(examplesNegCell{:});
 
 pobj = pos/(pos+neg);
 
