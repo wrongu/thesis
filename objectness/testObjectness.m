0498be7b438e750da6058b304b8999f093825cbf
wrongu <richard.d.lange.13@dartmouth.edu>
2013-05-21 16:19:25 -0400

    updated defaultParams

--- objectness/testObjectness.m
+++ objectness/testObjectness.m
@@ -1,4 +1,4 @@
-function testObjectness(testDirectory, params)
+function tests = testObjectness(testDirectory, params)
 % testObjectness(testDirectory) test the objectness measure according to
 % params.cues
 %
@@ -14,8 +14,29 @@ else
     structGT = ld.structGT;
 end
 
-for i=1:length(structGT)
+tests(1:4, length(structGT)) = struct('W', 0, 'boxes', [], 'percent', 0);
+for t = 1:size(tests,1)
+    for s = 1:length(structGT)
+        tests(t,s).W = 10^(t-1);
+        tests(t,s).descGT = structGT(s);
+    end
+end
+
+for t = 1:numel(tests)
+    tests(t).boxes = runObjectness(tests(t).descGT, tests(t).W, params);
     
+    count_valid = 0;
+    for box = 1:size(tests(t).boxes, 1)
+        for annot = 1:size(tests(t).descGT.boxes, 1)
+            if computePascalScore(tests(t).boxes(box,:), ...
+                    tests(t).descGT.boxes(annot,:) >= params.pascalThreshold)
+                count_valid = count_valid + 1;
+                break;
+            end 
+        end
+    end
+    
+    tests(t).percent = count_valid / size(tests(t).boxes, 1);
 end
 
 end
